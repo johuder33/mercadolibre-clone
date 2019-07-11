350d7396e85d05455762549d7f917aa9fa31e6b8
Juorder <johudergb@hotmail.com>
2019-07-11 01:01:13 -0400

    add parsers to response with right structure json

--- src/server/api/routes/search/index.ts
+++ src/server/api/routes/search/index.ts
@@ -1,14 +1,15 @@
 import request, { Response } from 'request';
 import { API_SEARCH_URL } from '../../constants';
 import { toJSON } from '../../utils';
+import { getOwnStructureResponse } from '../../parsers/search';
 
 export const search = (req: any, res: any) => {
   const { q: query } = req.query;
-  request(`${API_SEARCH_URL}?q=${query}`, (err: any, _: Response, body: any) => {
+  request(`${API_SEARCH_URL}?q=${query}&offset=0&limit=4`, (err: any, _: Response, body: any) => {
     if (err) {
       return res.status(400).json({ error: err });
     }
 
-    res.json(toJSON(body));
+    res.json(getOwnStructureResponse(toJSON(body)));
   });
 };
--- src/server/api/utils.ts
+++ src/server/api/utils.ts
@@ -1,1 +1,28 @@
+import pkgJSON from '../../../package.json';
+
+interface PkgJSON {
+  author: string;
+}
+
+interface Author {
+  name: string;
+  lastname: string;
+}
+
 export const toJSON = (objString: string) => JSON.parse(objString);
+
+const loadAuthorInformation = (pkgObject: PkgJSON) => {
+  let author: Author;
+
+  if (pkgObject) {
+    const { author: rawAuthor } = pkgObject;
+    const [name, lastname] = rawAuthor.split(/\s/);
+    author = { name, lastname };
+  }
+
+  return () => {
+    return author;
+  }
+};
+
+export const getAuthor = loadAuthorInformation(pkgJSON);
